a6538998227a70d338afd7a1643e23d81ae4f0ec
fergardm <97884151+fergardm@users.noreply.github.com>
2025-03-26 18:26:45 +0000

    big changes

--- src/BlazorApp/Models/EducationCertifs.cs
+++ src/BlazorApp/Models/EducationCertifs.cs
@@ -4,4 +4,6 @@ public class EducationCertifs
 {
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public string Logo { get; set; } = string.Empty;
 }
