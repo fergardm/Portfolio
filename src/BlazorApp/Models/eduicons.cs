10882602469ef878a58ee9cfc4f37cfbca9c96f0
fergardm <97884151+fergardm@users.noreply.github.com>
2025-03-26 13:59:31 +0000

    new icons and sections

---
+++ src/BlazorApp/Models/eduicons.cs
@@ -0,0 +1,10 @@
+namespace BlazorApp.Models;
+
+public class Eduicons
+{
+    public string Us { get; set; } = string.Empty;
+    public string Unia { get; set; } = string.Empty;
+    public string Cambridge { get; set; } = string.Empty;
+    public string Goethe { get; set; } = string.Empty;
+    public string Hsk { get; set; } = string.Empty;
+}
---
+++ src/BlazorApp/Models/languageCertifs.cs
@@ -0,0 +1,7 @@
+namespace BlazorApp.Models;
+
+public class LanguageCertifs
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
